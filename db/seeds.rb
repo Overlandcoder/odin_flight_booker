72c24e9ccb23c780af54cfc05600bcea1c5d9ab6
Overlander <anuragnandal@hotmail.com>
2022-08-28 19:29:39 -0400

    Create several airports

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+airports = Airport.create([{ code: "YYZ" }, { code: "LGW" }, { code: "LHR" }])
