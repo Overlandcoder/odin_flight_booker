98f60b328dd346aa620e6280c1f29a5555b12b57
Overlander <anuragnandal@hotmail.com>
2022-08-28 22:29:27 -0400

    Add #index, #new and #create actions, and #flight_params method

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,24 @@
 class FlightsController < ApplicationController
+  def index
+    @flights = Flight.all
+  end
+
+  def new
+    @flight = Flight.new
+  end
+
+  def create
+    @flight = Flight.new(flight_params)
+
+    if @flight.save
+      redirect_to @flight
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def flight_params
+    params.require(:flight).permit(:time, :duration)
+  end
 end
