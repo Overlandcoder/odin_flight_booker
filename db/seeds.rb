e0c3f98f039f49728622f1fc399e4e17d6fe4aac
Overlander <anuragnandal@hotmail.com>
2022-08-29 10:41:55 -0400

    Add several flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 airports = Airport.create([{ code: "YYZ" }, { code: "LGW" }, { code: "LHR" }])
+flights = Flight.create(
+  {
+    date_and_time: Time.new(2022, 10, 15, 14, 30),
+    duration: 10,
+    departure_airport_id: 4,
+    arrival_airport_id: 5
+  },
+  {
+    date_and_time: Time.new(2022, 11, 25, 12, 40),
+    duration: 5,
+    departure_airport_id: 3,
+    arrival_airport_id: 5
+  },
+  {
+    date_and_time: Time.new(2023, 1, 10, 9, 30),
+    duration: 4,
+    departure_airport_id: 3,
+    arrival_airport_id: 4
+  },
+  {
+    date_and_time: Time.new(2022, 12, 10, 10, 50),
+    duration: 6,
+    departure_airport_id: 4,
+    arrival_airport_id: 3
+  }
+  ])
