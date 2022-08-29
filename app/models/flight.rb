63b9d02577104f25f36073e366d3af3e4b413eb6
Overlander <anuragnandal@hotmail.com>
2022-08-28 22:21:44 -0400

    Add validations for flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+  validates :time, presence: true
+  validates :duration, presence: true
 end
