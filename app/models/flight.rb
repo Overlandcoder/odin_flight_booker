be5191accaf7ff008e2d2213d1dca4526f04b413
Overlander <anuragnandal@hotmail.com>
2022-08-30 16:06:54 -0400

    Add scopes and user_search method

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,4 +4,12 @@ class Flight < ApplicationRecord
 
   validates :departure_time, presence: true
   validates :duration, presence: true
+
+  scope :same_departure, -> { where("departure_airport_id == ?", [:departure_airport_id]) }
+  scope :same_arrival, -> { where("arrival_airport_id == ?", [:arrival_airport_id]) }
+  scope :same_time, -> { where("departure_time == ?", [:flight_time]) }
+
+  def self.user_search(query_params)
+    same_departure.same_arrival.same_time
+  end
 end
