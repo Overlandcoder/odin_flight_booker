488a340841b37b73d9bd53d6dea133635e9617f8
Overlander <anuragnandal@hotmail.com>
2022-08-30 21:16:04 -0400

    Use single instead of multiple where statements

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -6,9 +6,9 @@ class Flight < ApplicationRecord
   validates :duration, presence: true
 
   def self.user_search(query_params)
-    where(departure_airport_id: query_params[:departure_airport_id]).
-    where(arrival_airport_id: query_params[:arrival_airport_id]).
-    where(departure_time: query_params[:departure_time])
+    where(departure_airport_id: query_params[:departure_airport_id],
+          arrival_airport_id: query_params[:arrival_airport_id],
+          departure_time: query_params[:departure_time])
   end
 
   def departure_date_formatted
