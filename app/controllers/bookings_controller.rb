3836f7132ca2b5f862fa3fddcef52f1947f83e16
Overlander <anuragnandal@hotmail.com>
2022-09-01 12:20:17 -0400

    Add flight and passengers instance variables

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,7 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @flight = Flight.find(params[:flight_id])
+    @passengers = params[:passengers]
   end
 end
