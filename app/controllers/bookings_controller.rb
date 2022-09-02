fe905a41ebf38b11680bdda1b519114099c6ba78
Overlander <anuragnandal@hotmail.com>
2022-09-01 21:49:55 -0400

    Add #booking_params method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,12 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
-    @flight = Flight.find(params[:flight_id])
+    @flight = Flight.find(booking_params[:flight_id])
     @passengers = params[:passengers]
+  end
+
+  private
+  def booking_params
+    params.permit(:flight_id, :passengers, :customer, :commit)
   end
 end
