ad15d44fb04359bae54b2b82f8a320581cb01e28
Overlander <anuragnandal@hotmail.com>
2022-09-03 16:36:00 -0400

    Implement new booking form which creates new booking and passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,25 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @flight = Flight.find(params[:flight_id])
+    @num_of_passengers = params[:num_of_passengers]
+    @booking.passengers.build
+  end
+
+  def create
     @flight = Flight.find(booking_params[:flight_id])
-    @passengers = params[:passengers]
+    @booking = @flight.bookings.build(booking_params)
+
+    if @booking.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
   def booking_params
-    params.permit(:flight_id, :passengers, :customer, :commit)
+    params.require(:booking).permit(:flight_id,
+                                    passengers_attributes: [:name, :email])
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   has_many :passengers
   belongs_to :flight
-  accepts_nested_attributes_for :customer
+  accepts_nested_attributes_for :passengers
 end
