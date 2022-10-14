3eeb07572cde024d6ec91a313fb84fe153d70409
Overlander <anuragnandal@hotmail.com>
2022-10-14 15:00:50 -0400

    Add city column to airports table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_02_235834) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_14_190026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_02_235834) do
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
   end
 
   create_table "bookings", force: :cascade do |t|
