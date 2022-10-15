8c1915e216e777a35139b8a13bf7b2e8b6d46a0d
Overlander <anuragnandal@hotmail.com>
2022-10-15 08:05:42 -0400

    Add many more flights

--- app/services/flight_generator.rb
+++ app/services/flight_generator.rb
@@ -4,6 +4,22 @@ class FlightGenerator < ApplicationService
   end
 
   def call
+    airport_pairs.each do |pair|
+      airport1 = Airport.find_by(code: pair[0])
+      airport2 = Airport.find_by(code: pair[1])
 
+      Flight.create(departure_time: random_time, duration: rand(3..15),
+                    departure_airport_id: airport1.id,
+                    arrival_airport_id: airport2.id)
+    end
+  end
+
+  def random_time
+    Faker::Time.between_dates(from: @date, to: @date, period: :all)
+  end
+
+  def airport_pairs
+    ["YYZ", "LGW", "JFK", "ATL", "DFW",
+    "DEN", "ORD", "LAS", "PHX", "MIA"].permutation(2)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,45 +8,15 @@
 
 airports = Airport.create([
   { code: "YYZ", city: "Toronto" }, { code: "LGW", city: "London" },
-  { code: "LAX", city: "Los Angeles" }, { code: "SFO", city: "San Francisco" },
   { code: "JFK", city: "New York" }, { code: "ATL", city: "Atlanta" },
   { code: "DFW", city: "Dallas-Fort Worth" }, { code: "DEN", city: "Denver" },
   { code: "ORD", city: "Chicago" }, { code: "LAS", city: "Las Vegas" },
   { code: "PHX", city: "Phoenix" }, { code: "MIA", city: "Miami" },
-  { code: "DXB", city: "Dubai" }, { code: "FLL", city: "Fort Lauderdale" },
-  { code: "AMS", city: "Amsterdam" }, { code: "DME", city: "Moscow" }
   ])
 
-flights = Flight.create([
-  {
-    departure_time: "2022-12-20 07:00:00",
-    duration: 10,
-    departure_airport_id: 1,
-    arrival_airport_id: 2
-  },
-  {
-    departure_time: "2022-11-15 10:30:00",
-    duration: 5,
-    departure_airport_id: 1,
-    arrival_airport_id: 3
-  },
-  {
-    departure_time: "2022-11-21 19:30:00",
-    duration: 4,
-    departure_airport_id: 2,
-    arrival_airport_id: 3
-  },
-  {
-    departure_time: "2023-01-20 15:00:00",
-    duration: 6,
-    departure_airport_id: 2,
-    arrival_airport_id: 1
-  }
-  ])
+today = Date.today
+month_from_today = Date.today + 30
 
-start = Time.now.day
-finish = Time.now.day + 31
-
-(start..finish).each do |date|
+(today..month_from_today).each do |date|
   FlightGenerator.call(date)
 end
