079cce8990f7dd716f5f7441993f3034e71dfa53
Overlander <anuragnandal@hotmail.com>
2022-10-17 11:21:03 -0400

    Improve search results form layout and style

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -14,10 +14,11 @@ class Flight < ApplicationRecord
   end
 
   def departure_time_formatted
-    departure_time.strftime("%I:%M %P")
+    departure_time.strftime("%l:%M %P")
   end
 
   def flight_info
-    "#{departure_airport.city} to #{arrival_airport.city} - #{departure_date} #{departure_time_formatted}"
+    "#{departure_airport.city} to #{arrival_airport.city} on #{departure_date}
+     at #{departure_time_formatted}. Duration: #{duration.to_i} hours"
   end
 end
